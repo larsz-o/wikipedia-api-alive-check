c1b2cb0dc1d86d110f40d843b1b0126a47661295
Lars <larsmackenzie@gmail.com>
2019-08-05 14:32:47 -0400

    edited my search function to be accurate

--- scripts.js
+++ scripts.js
@@ -5,19 +5,17 @@ let aliveStatus = true;
 
 xhr.onload = function (){
    if (xhr.status >= 200 && xhr.status < 300){
-       console.log('success!')
        let response = xhr.responseText;
-       console.log(response);
-       if(response.search('died') !== -1){
+       if(response.search('died') === -1){
            aliveStatus = true;
-           console.log('yay!')
        } else {
            aliveStatus = false;
        }
    } else {
        console.log('error', )
    }
+   console.log('David is alive: ' + aliveStatus)
 }
 
-xhr.open('GET', 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=David_Schwimmer&section=1' );
+xhr.open('GET', 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=David_Schwimmer&section=0' );
 xhr.send();
